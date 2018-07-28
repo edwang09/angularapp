e927105799768a28ead5d8d6c691b679bce253cc
Jianan Wang123 <edwang09@gmail.com>
2018-07-28 01:55:22 -0400

    redesign landing and footer

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -8,7 +8,7 @@ import { Post } from '../../models/post'
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
   calendar:Data
